3617f137f7e6be0b3bebe28e831114be40fb5ac3
fahima345 <cfahima26@gmail.com>
2022-12-11 09:03:51 -0500

    First message

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cfahi
+ *
+ */
+module practice1 {
+}
---
+++ src/practice1/example.java
@@ -0,0 +1,15 @@
+package practice1;
+
+public class example {
+
+	public static void main(String[] args) 
+	{
+		System.out.print("ja");
+		System.out.print("va");
+		System.out.println("my name is fahima");
+		System.out.print("hello wolrd");
+
+
+	}
+
+}
